e46ef3d6167e53ad6f8c1d31016a810b76ef508a
Stefan Sauer <ensonic@users.sf.net>
2021-10-04 21:27:33 +0200

    Change site to docs, since that is hard coded for pages.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ run `./build.sh`, requires m4
 
 ## preview
 ```shell
-cd site
+cd docs
 python3 -m http.server
 xdg-open index.html
 ```
--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ DEPS="index.m4.de index.m4.en error.m4 $(find ger -name '*.m4') $(find eng -name
 for in in ${DEPS}; do \
 	out=$(echo "${in}" | sed -e 's/.m4/.html/g')
 	echo "[gen] ${in}" '->' "${out}"
-	m4 -P -I_m4 "${in}" >"site/${out}"
+	m4 -P -I_m4 "${in}" >"docs/${out}"
 done
 
